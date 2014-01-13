0ea0009d262d2d88d23715b01e60c2adc4923b72
Scott Harvey <scottfharvey@gmail.com>
2014-01-13 02:45:35 +0000

    Added html into js

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello everyone2!');
+buffer = fs.readFileSync(index.html);
+string = Buffer.toString(buffer);
+
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
