6d7d2508716aaa11188a4ac7e7f1b65af256265d
Scott Harvey <scottfharvey@gmail.com>
2014-03-17 02:20:19 +0000

    updated port 808

--- web.js
+++ web.js
@@ -1,14 +1,14 @@
 var express = require('express');
-var app = express();
 var fs = require('fs');
-app.use(express.logger());
+var htmlfile = "index.html";
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var data = fs.readFileSync("index.html","utf-8");
+var data = fs.readFileSync(htmlfile).toString();
   response.send(data);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
